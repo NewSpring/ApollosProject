5bd172bef90e06ddb97d621754ea3b671bd39144
Isaac Hardy <isaac.hardy@newspring.cc>
2019-04-03 11:40:04 -0400

    Updated Bundle Id and App name (#5)
    * renamed
    
    * updated bundler and name

---
+++ packages/newspringchurchapp/android/app/src/main/java/com/newspringchurchapp/MainApplication.java
@@ -0,0 +1,75 @@
+package com.newspringchurchapp;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.mybdesign.RNPassKit.RNPassKitPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.horcrux.svg.SvgPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.swmansion.rnscreens.RNScreenPackage;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
+import com.tanguyantoine.react.MusicControl;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.github.droibit.android.reactnative.customtabs.CustomTabsPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNPassKitPackage(),
+            new MapsPackage(),
+            new RNFetchBlobPackage(),
+            new RNCWebViewPackage(),
+            new ReactVideoPackage(),
+            new SvgPackage(),
+            new SplashScreenReactPackage(),
+            new RNScreenPackage(),
+            new ReactNativeOneSignalPackage(),
+            new MusicControl(),
+            new LinearGradientPackage(),
+            new ImagePickerPackage(),
+            new RNDeviceInfo(),
+            new CustomTabsPackage(),
+            new ReactNativeConfigPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
---
+++ packages/newspringchurchapp/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+// import { Sentry } from 'react-native-sentry';
+
+import { BackgroundView, withTheme } from '@apollosproject/ui-kit';
+import Passes from '@apollosproject/ui-passes';
+
+import MediaPlayer from 'newspringchurchapp/src/ui/MediaPlayer';
+import Auth from '@apollosproject/ui-auth';
+import Providers from './Providers';
+import NavigationService from './NavigationService';
+import ContentSingle from './content-single';
+import Tabs from './tabs';
+import PersonalDetails from './user-settings/PersonalDetails';
+import ChangePassword from './user-settings/ChangePassword';
+import Location from './user-settings/Locations';
+import UserWebBrowser from './user-web-browser';
+import Onboarding from './onboarding';
+
+// Sentry.config(
+//   'https://5908fa19ed37447f86b2717423cadec5:45dd3b58792b413cb67109c5e63a0bb7@sentry.io/1241658'
+// ).install();
+
+const AppStatusBar = withTheme(({ theme }) => ({
+  barStyle: 'dark-content',
+  backgroundColor: theme.colors.paper,
+}))(StatusBar);
+
+const AppNavigator = createStackNavigator(
+  {
+    Tabs,
+    ContentSingle,
+    Auth,
+    PersonalDetails,
+    ChangePassword,
+    Location,
+    Passes,
+    UserWebBrowser,
+    Onboarding,
+  },
+  {
+    initialRouteName: 'Tabs',
+    mode: 'modal',
+    headerMode: 'screen',
+  }
+);
+
+const App = () => (
+  <Providers>
+    <BackgroundView>
+      <AppStatusBar barStyle="dark-content" />
+      <AppNavigator
+        ref={(navigatorRef) => {
+          NavigationService.setTopLevelNavigator(navigatorRef);
+        }}
+      />
+      <MediaPlayer />
+    </BackgroundView>
+  </Providers>
+);
+
+export default App;
