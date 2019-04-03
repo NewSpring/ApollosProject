77b6ec51ff6e264ee9c6f6b56762bb09916be96e
Isaac Hardy <isaac.hardy@newspring.cc>
2019-04-03 16:13:14 -0400

    updated summary datasource

--- packages/apollos-church-api/src/data/content-item/data-source.js
+++ packages/apollos-church-api/src/data/content-item/data-source.js
@@ -1,3 +1,20 @@
-import { ContentItem } from '@apollosproject/data-connector-rock';
+import { ContentItem as oldContentItem } from '@apollosproject/data-connector-rock';
+import natural from 'natural';
+import sanitizeHtmlNode from 'sanitize-html';
 
-export default ContentItem.dataSource;
+export default class ContentItem extends oldContentItem.dataSource {
+  createSummary = ({ content, attributeValues: { summary = {} } }) => {
+    if (summary.value) return summary.value;
+    if (!content || typeof content !== 'string') return '';
+    // Protect against 0 length sentences (tokenizer will throw an error)
+    if (content.split(' ').length === 1) return '';
+
+    const tokenizer = new natural.SentenceTokenizer();
+    return tokenizer.tokenize(
+      sanitizeHtmlNode(content, {
+        allowedTags: [],
+        allowedAttributes: [],
+      })
+    )[0];
+  };
+}
--- packages/apollos-church-api/src/data/content-item/resolver.js
+++ packages/apollos-church-api/src/data/content-item/resolver.js
@@ -2,9 +2,21 @@ import { ContentItem } from '@apollosproject/data-connector-rock';
 import { schemaMerge } from '@apollosproject/server-core';
 
 const resolver = {
-  ContentItem: {
-    ...ContentItem.resolver.ContentItem,
-  },
+  // ContentItem: {
+  //   htmlContent: (data) => console.log(data) || 'HELLLLLO',
+  // },
+  // MediaContentItem: {
+  //   htmlContent: (data) => console.log(data) || 'HELLLLLO',
+  // },
+  // ContentSeriesContentItem: {
+  //   htmlContent: (data) => console.log(data) || 'HELLLLLO',
+  // },
+  // UniversalContentItem: {
+  //   htmlContent: (data) => console.log(data) || 'HELLLLLO',
+  // },
+  // DevotionalContentItem: {
+  //   htmlContent: (data) => console.log(data) || 'HELLLLLO',
+  // },
 };
 
 export default schemaMerge(resolver, ContentItem);
