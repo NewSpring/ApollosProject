15cfbbc9ab596d6f3a0a4918a1f564ddfb0a88da
Michael Neeley <micneeley14@gmail.com>
2019-04-08 12:18:44 -0400

    added some stuff

--- packages/apollos-data-prayer/src/AddPrayer/index.js
+++ packages/apollos-data-prayer/src/AddPrayer/index.js
@@ -1,9 +1,28 @@
 import React, { memo } from 'react';
-import { Image, Text } from 'react-native';
+import { Image } from 'react-native';
 import PropTypes from 'prop-types';
+import { H2, H5, FlexedView, styled } from '@apollosproject/ui-kit';
+
+const Content = styled({
+  justifyContent: 'center',
+})(FlexedView);
+
+const Title = styled(({ theme }) => ({
+  color: theme.colors.primary,
+  marginBottom: theme.sizing.baseUnit * 0.5,
+}))(H2);
+
+const Description = styled(({ theme }) => ({
+  color: theme.colors.text.secondary,
+  marginBottom: theme.sizing.baseUnit,
+}))(H5);
 
 const AddPrayer = memo(({ imgSrc, title, description }) => (
-  <Text>Hello, world!</Text>
+  <Content>
+    <Image source={imgSrc} />
+    <Title>{title}</Title>
+    <Description>{description}</Description>
+  </Content>
 ));
 
 AddPrayer.propTypes = {
@@ -17,5 +36,7 @@ AddPrayer.defaultProps = {
   description:
     "There aren't currently any prayers in your community. Send them one now.",
 };
+
+AddPrayer.displayName = 'AddPrayer';
 
 export default AddPrayer;
