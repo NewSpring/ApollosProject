f7c90134ba92c7d819aef5dad4bfbe7fa4cd7b5e
Michael Neeley <micneeley14@gmail.com>
2019-04-08 13:45:24 -0400

    added story

---
+++ packages/newspringchurchapp/src/prayer/AddPrayer/AddPrayer.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+
+import AddPrayer from '.';
+
+storiesOf('prayer/AddPrayer', module).add('default', () => <AddPrayer />);
