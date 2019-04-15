50a8211256733c160a69204f8b16a07937a5434b
Michael Neeley <micneeley14@gmail.com>
2019-04-15 15:27:38 -0400

    added some more tests

--- packages/newspringchurchapp/src/prayer/AddPrayer/AddPrayerCard/AddPrayerCard.tests.js
+++ packages/newspringchurchapp/src/prayer/AddPrayer/AddPrayerCard/AddPrayerCard.tests.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import AddPrayerHeader from '.';
+import AddPrayerCard from '.';
 
-describe('The AddPrayerHeader component', () => {
+describe('The AddPrayerCard component', () => {
   it('should render', () => {
     /* we have to pass in a date via defaultDate or the DateInput component will create a current date
      * object and invalidate the snapshots every time. */
-    const tree = renderer.create(<AddPrayerHeader />);
+    const tree = renderer.create(<AddPrayerCard />);
     expect(tree).toMatchSnapshot();
   });
   it('should render a custom avatar', () => {
     /* we have to pass in a date via defaultDate or the DateInput component will create a current date
      * object and invalidate the snapshots every time. */
     const tree = renderer.create(
-      <AddPrayerHeader imgSrc={{ uri: 'https://picsum.photos/55/55?random' }} />
+      <AddPrayerCard imgSrc={{ uri: 'https://picsum.photos/55/55?random' }} />
     );
     expect(tree).toMatchSnapshot();
   });
   it('should render a custom title', () => {
     /* we have to pass in a date via defaultDate or the DateInput component will create a current date
      * object and invalidate the snapshots every time. */
-    const tree = renderer.create(<AddPrayerHeader title={'custom title'} />);
+    const tree = renderer.create(
+      <AddPrayerCard
+        description={'this is a custom description for the card.'}
+      />
+    );
     expect(tree).toMatchSnapshot();
   });
 });
--- packages/newspringchurchapp/src/prayer/AddPrayer/AddPrayerForm/AddPrayerForm.tests.js
+++ packages/newspringchurchapp/src/prayer/AddPrayer/AddPrayerForm/AddPrayerForm.tests.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import AddPrayerHeader from '.';
+import AddPrayerForm from '.';
 
-describe('The AddPrayerHeader component', () => {
+describe('The AddPrayerForm component', () => {
   it('should render', () => {
     /* we have to pass in a date via defaultDate or the DateInput component will create a current date
      * object and invalidate the snapshots every time. */
-    const tree = renderer.create(<AddPrayerHeader />);
+    const tree = renderer.create(<AddPrayerForm navigation={jest.fn()} />);
     expect(tree).toMatchSnapshot();
   });
   it('should render a custom avatar', () => {
     /* we have to pass in a date via defaultDate or the DateInput component will create a current date
      * object and invalidate the snapshots every time. */
     const tree = renderer.create(
-      <AddPrayerHeader imgSrc={{ uri: 'https://picsum.photos/55/55?random' }} />
+      <AddPrayerForm
+        navigation={jest.fn()}
+        imgSrc={{ uri: 'https://picsum.photos/55/55?random' }}
+      />
     );
     expect(tree).toMatchSnapshot();
   });
   it('should render a custom title', () => {
     /* we have to pass in a date via defaultDate or the DateInput component will create a current date
      * object and invalidate the snapshots every time. */
-    const tree = renderer.create(<AddPrayerHeader title={'custom title'} />);
+    const tree = renderer.create(
+      <AddPrayerForm navigation={jest.fn()} btnLabel={'custom button label'} />
+    );
     expect(tree).toMatchSnapshot();
   });
 });
