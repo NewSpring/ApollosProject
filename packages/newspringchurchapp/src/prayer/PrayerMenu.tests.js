29aa70865da7d7ab5759520af913d9c75bcfc39b
richarddubay <richardtdubayjr@gmail.com>
2019-04-24 13:19:22 -0400

    fixed test

--- packages/newspringchurchapp/src/prayer/PrayerMenu.tests.js
+++ packages/newspringchurchapp/src/prayer/PrayerMenu.tests.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+import Providers from 'newspringchurchapp/src/Providers';
 import PrayerMenu from '.';
 
 it('renders prayer without crashing', () => {
-  const rendered = renderer.create(<PrayerMenu />).toJSON();
+  const rendered = renderer
+    .create(
+      <Providers>
+        <PrayerMenu />
+      </Providers>
+    )
+    .toJSON();
   expect(rendered).toBeTruthy();
 });
