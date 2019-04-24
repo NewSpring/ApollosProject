cb1ca8360480de60c31dc304ea4bbce4586e685d
Richard Dubay <richardtdubayjr@gmail.com>
2019-04-24 14:35:53 -0400

    Fix Prayer Menu (#21)
    * upgraded react-native-tab-view
    
    * jest mocking FTW
    
    * updated snapshots

--- packages/newspringchurchapp/jest.setup.js
+++ packages/newspringchurchapp/jest.setup.js
@@ -66,6 +66,72 @@ jest.mock('@apollosproject/ui-analytics', () => ({
 
 jest.mock('react-native-video', () => 'Video');
 
+jest.mock('react-native-reanimated', () => ({
+  Value: jest.fn(),
+  onChange: jest.fn(),
+  abs: jest.fn(),
+  add: jest.fn(),
+  and: jest.fn(),
+  block: jest.fn(),
+  call: jest.fn(),
+  clockRunning: jest.fn(),
+  cond: jest.fn(),
+  divide: jest.fn(),
+  eq: jest.fn(),
+  event: jest.fn(),
+  greaterThan: jest.fn(),
+  max: jest.fn(),
+  min: jest.fn(),
+  multiply: jest.fn(),
+  neq: jest.fn(),
+  or: jest.fn(),
+  not: jest.fn(),
+  round: jest.fn(),
+  set: jest.fn(),
+  spring: jest.fn(),
+  startClock: jest.fn(),
+  stopClock: jest.fn(),
+  sub: jest.fn(),
+  timing: jest.fn(),
+  Easing: { out: jest.fn() },
+  Clock: jest.fn(),
+  ...require.requireActual('Animated'),
+}));
+
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+  };
+});
+
 jest.mock('NativeEventEmitter');
 
 jest.mock('react-native-maps');
