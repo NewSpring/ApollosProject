45cb0a4a780c6ddb2b2a73618ddd6251fc32831c
Vincent Wilson <vince@classyh.at>
2019-04-26 12:06:34 -0500

    Added API side bugsnag integration

---
+++ packages/apollos-church-api/src/bugsnag.js
@@ -0,0 +1,13 @@
+import bugsnag from '@bugsnag/js';
+import bugsnagExpress from '@bugsnag/plugin-express';
+
+const bugsnagClient = bugsnag({
+  apiKiey: process.env.BUGSNAG_API_KEY,
+  notifyReleaseStages: ['production', 'staging'], // don't send errors in dev or test.
+});
+
+bugsnagClient.use(bugsnagExpress);
+
+const bugsnagMiddleware = bugsnagClient.getPlugin('express');
+
+export { bugsnagClient as default, bugsnagMiddleware };
