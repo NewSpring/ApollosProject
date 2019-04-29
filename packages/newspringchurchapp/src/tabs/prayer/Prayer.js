c3e26b64121464e9d3bfbdd6380afaec7f49a0aa
richarddubay <richardtdubayjr@gmail.com>
2019-04-29 09:41:38 -0400

    fixed my linting problems

--- packages/newspringchurchapp/src/tabs/prayer/Prayer.js
+++ packages/newspringchurchapp/src/tabs/prayer/Prayer.js
@@ -1,18 +1,15 @@
-import React, { PureComponent } from 'react';
-import { SafeAreaView, ScrollView, Dimensions } from 'react-native';
-import { BackgroundView, styled } from '@apollosproject/ui-kit';
-import PrayerMenu from '../../prayer';
+import React, { PureComponent } from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import { BackgroundView, styled } from "@apollosproject/ui-kit";
+import PrayerMenu from "../../prayer";
 
-// const { height } = Dimensions.get('window');
 const Header = styled(({ theme }) => ({
-  backgroundColor: theme.colors.background.paper,
+  backgroundColor: theme.colors.background.paper
 }))(ScrollView);
-// marginTop: theme.sizing.baseUnit * 6,
-// height,
 
 class Prayer extends PureComponent {
   static navigationOptions = () => ({
-    header: null,
+    header: null
   });
 
   render() {
