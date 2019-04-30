0cb517309421ce13f578bf3c2cef4b60c75fd044
Richard Dubay <richardtdubayjr@gmail.com>
2019-04-30 15:52:41 -0400

    Update packages/newspringchurchapp/src/prayer/PrayerCard/PrayerCard.tests.js
    Co-Authored-By: cdanwards <dan.edwards@newspring.cc>

--- packages/newspringchurchapp/src/prayer/PrayerCard/PrayerCard.tests.js
+++ packages/newspringchurchapp/src/prayer/PrayerCard/PrayerCard.tests.js
@@ -10,7 +10,7 @@ const cardData = {
     uri: 'https://fillmurray.com/400/600',
   },
   name: 'Bill',
-  prayer:
+  text:
     'I want to go back to school for my PTA degree next year, but I’m afraid of not getting accepted. I have a great job right now but I’m not very happy. Pray my plans for school align with God’s plans for me.',
   source: 'Anderson',
 };
