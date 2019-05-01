c12e6d25d89e1fb0de5f5426afc3773361f7aca0
Isaac Hardy <isaac.hardy@newspring.cc>
2019-05-01 11:08:08 -0400

    moved react-nav location

--- packages/newspringchurchapp/src/index.js
+++ packages/newspringchurchapp/src/index.js
@@ -8,7 +8,6 @@ import Passes from '@apollosproject/ui-passes';
 import MediaPlayer from 'newspringchurchapp/src/ui/MediaPlayer';
 import Auth, { ProtectedRoute } from '@apollosproject/ui-auth';
 
-import PrayerList from './prayer/PrayerList';
 import Providers from './Providers';
 import NavigationService from './NavigationService';
 import ContentSingle from './content-single';
@@ -40,7 +39,6 @@ const AppNavigator = createStackNavigator(
     UserWebBrowser,
     Onboarding,
     Prayer,
-    PrayerList,
   },
   {
     initialRouteName: 'ProtectedRoute',
