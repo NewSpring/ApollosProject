28c7190b810f0a7c86f6dc443baf7543288c4e63
Michael Neeley <micneeley14@gmail.com>
2019-05-01 09:59:38 -0400

    added some prayer menu filters

--- packages/apollos-data-prayer/src/prayer-menu-categories/data-source.js
+++ packages/apollos-data-prayer/src/prayer-menu-categories/data-source.js
@@ -5,13 +5,21 @@ import ApollosConfig from '@apollosproject/config';
 const { ROCK_MAPPINGS } = ApollosConfig;
 
 class PrayerMenuCategory extends ContentItem.dataSource {
-  getPrayerMenuCategories() {
-    return this.request()
+  getPrayerMenuCategories = async () => {
+    const allCategories = this.request()
       .filter(
         `ContentChannelId eq ${ROCK_MAPPINGS.PRAYER_MENU_CATEGORIES_CHANNEL_ID}`
       )
       .get();
-  }
+    // don't show campus category if they don't have one
+    const {
+      dataSources: { Auth },
+    } = this.context;
+    const currentPerson = await Auth.getCurrentPerson();
+    // TODO: filter on whether they have a campus or group
+    console.log('person', currentPerson);
+    return allCategories;
+  };
 }
 
 export default PrayerMenuCategory;
