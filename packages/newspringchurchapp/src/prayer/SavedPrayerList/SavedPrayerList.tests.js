b06d90ecb676e18b0bd3ede45e407dd1c6e77f0e
richarddubay <richardtdubayjr@gmail.com>
2019-05-02 11:06:54 -0400

    last ditch effort to get the test to pull some data

--- packages/newspringchurchapp/src/prayer/SavedPrayerList/SavedPrayerList.tests.js
+++ packages/newspringchurchapp/src/prayer/SavedPrayerList/SavedPrayerList.tests.js
@@ -1,48 +1,35 @@
 import React from 'react';
-
 import renderer from 'react-test-renderer';
+import wait from 'waait';
 import Providers from 'newspringchurchapp/src/Providers';
 import getSavedPrayers from '../data/queries/getSavedPrayers';
 import SavedPrayerList from '.';
-
-const savedPrayers = [
-  {
-    id: 'PrayerRequest:b99bbd5002a0bf8d4c73e86afe9d4786',
-    firstName: 'Dan',
-    lastName: 'Edwards',
-    text: 'Praying for an around the world trip to happen sometime in 2020.',
-    enteredDateTime: '2019-05-02T09:00:00.000',
-  },
-  {
-    id: 'PrayerRequest:b99bbd5002a0bf8d4c73e86afe9d4786',
-    firstName: 'Isaac',
-    lastName: 'Hardy',
-    text: 'I could really use some new Youtube subscribers to my channel.',
-    enteredDateTime: '2019-05-02T09:00:00.000',
-  },
-  {
-    id: 'PrayerRequest:b99bbd5002a0bf8d4c73e86afe9d4786',
-    firstName: 'Michael',
-    lastName: 'Neeley',
-    text: 'Guys, just make a decision already.',
-    enteredDateTime: '2019-05-02T09:00:00.000',
-  },
-  {
-    id: 'PrayerRequest:b99bbd5002a0bf8d4c73e86afe9d4786',
-    firstName: 'Rich',
-    lastName: 'Dubay',
-    text: 'I just want to live in a cave.',
-    enteredDateTime: '2019-05-02T09:00:00.000',
-  },
-];
 
 const mock = {
   request: {
     query: getSavedPrayers,
   },
-  response: {
+  result: {
     data: {
-      savedPrayers,
+      savedPrayers: [
+        {
+          id: 'PrayerRequest:d13a81b4b14c4ae89db1a241c9d99a79',
+          campus: {
+            id: 'Campus:4f68015ba18662a7409d1219a4ce013e',
+            name: 'Anderson',
+          },
+          firstName: 'Isaac',
+          lastName: 'Hardy',
+          text: 'I could really use some new Youtube subscribers',
+          enteredDateTime: '2019-05-01T22:25:06.453',
+          person: {
+            photo: {
+              uri: null,
+            },
+          },
+          isAnonymous: false,
+        },
+      ],
     },
   },
 };
@@ -54,6 +41,7 @@ describe('the SavedPrayerList component', () => {
         <SavedPrayerList />
       </Providers>
     );
+    await wait(0); // wait for response from graphql
     expect(tree).toMatchSnapshot();
   });
 });
