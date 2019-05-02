9f87a7cab7006cfb91a528bb638481b97761f48d
richarddubay <richardtdubayjr@gmail.com>
2019-05-02 08:56:02 -0400

    wrote a test

---
+++ packages/newspringchurchapp/src/prayer/SavedPrayerList/SavedPrayerList.tests.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { renderWithApolloData } from 'newspringchurchapp/src/utils/testUtils';
+
+import Providers from 'newspringchurchapp/src/Providers';
+import getSavedPrayers from '../data/queries/getUserPrayers';
+import SavedPrayerList from '.';
+
+const savedPrayers = [
+  {
+    id: 'PrayerRequest:b99bbd5002a0bf8d4c73e86afe9d4786',
+    firstName: 'Dan',
+    lastName: 'Edwards',
+    text: 'Praying for an around the world trip to happen sometime in 2020.',
+    enteredDateTime: '2019-05-02T09:00:00.000',
+  },
+  {
+    id: 'PrayerRequest:b99bbd5002a0bf8d4c73e86afe9d4786',
+    firstName: 'Isaac',
+    lastName: 'Hardy',
+    text: 'I could really use some new Youtube subscribers to my channel.',
+    enteredDateTime: '2019-05-02T09:00:00.000',
+  },
+  {
+    id: 'PrayerRequest:b99bbd5002a0bf8d4c73e86afe9d4786',
+    firstName: 'Michael',
+    lastName: 'Neeley',
+    text: 'Guys, just make a decision already.',
+    enteredDateTime: '2019-05-02T09:00:00.000',
+  },
+  {
+    id: 'PrayerRequest:b99bbd5002a0bf8d4c73e86afe9d4786',
+    firstName: 'Rich',
+    lastName: 'Dubay',
+    text: 'I just want to live in a cave.',
+    enteredDateTime: '2019-05-02T09:00:00.000',
+  },
+];
+
+const mock = {
+  request: {
+    query: getSavedPrayers,
+  },
+  response: {
+    data: {
+      savedPrayers,
+    },
+  },
+};
+
+describe('the SavedPrayerList component', () => {
+  it('renders a list of saved prayers', async () => {
+    const tree = await renderWithApolloData(
+      <Providers mocks={[mock]}>
+        <SavedPrayerList />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
