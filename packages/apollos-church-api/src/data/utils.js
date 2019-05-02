af6067758cae24ac9ab7f6611deaeee34b4c5f48
Isaac Hardy <isaac.hardy@newspring.cc>
2019-05-02 15:38:55 -0400

    fixed

--- packages/apollos-church-api/src/data/utils.js
+++ packages/apollos-church-api/src/data/utils.js
@@ -3,8 +3,10 @@ import ApollosConfig from '@apollosproject/config';
 const { ROCK, ROCK_MAPPINGS } = ApollosConfig;
 
 export const createAssetUrl = (value) =>
-  `${ROCK.IMAGE_URL}/${
-    ROCK_MAPPINGS.ASSET_STORAGE_PROVIDERS[`${value.AssetStorageProviderId}`]
-  }/${value.Key}`;
+  value.path
+    ? value.path
+    : `${ROCK.IMAGE_URL}/${
+        ROCK_MAPPINGS.ASSET_STORAGE_PROVIDERS[`${value.AssetStorageProviderId}`]
+      }/${value.Key}`;
 
 export const fakeFunction = () => 'Fake';
