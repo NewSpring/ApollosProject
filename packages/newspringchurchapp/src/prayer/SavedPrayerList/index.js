a6c5e672491e909a48863f65ff0fded7383fea56
Michael Neeley <micneeley14@gmail.com>
2019-05-02 06:58:09 -0400

    Update packages/newspringchurchapp/src/prayer/SavedPrayerList/index.js
    Co-Authored-By: richarddubay <richardtdubayjr@gmail.com>

--- packages/newspringchurchapp/src/prayer/SavedPrayerList/index.js
+++ packages/newspringchurchapp/src/prayer/SavedPrayerList/index.js
@@ -56,7 +56,7 @@ const SavedPrayerList = () => (
                             },
                           });
                         },
-                        destructive: true,
+                        destructive: false,
                       },
                     ]}
                   />
