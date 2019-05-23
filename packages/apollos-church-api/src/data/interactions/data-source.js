23300268507d311e316f2293bb730dc931ed829f
Isaac Hardy <isaac.hardy@newspring.cc>
2019-05-23 16:16:21 -0400

    updated InteractionData

--- packages/apollos-church-api/src/data/interactions/data-source.js
+++ packages/apollos-church-api/src/data/interactions/data-source.js
@@ -1,6 +1,5 @@
 import { Interactions as apollosInteractions } from '@apollosproject/data-connector-rock';
-// import { parseGlobalId } from '@apollosproject/server-core';
-// import ApollosConfig from '@apollosproject/config';
+import ApollosConfig from '@apollosproject/config';
 
 export default class Interactions extends apollosInteractions.dataSource {
   async createPrayerRequestInteraction({ prayerId, operationName }) {
@@ -24,7 +23,9 @@ export default class Interactions extends apollosInteractions.dataSource {
       Operation: operationName,
       InteractionDateTime: new Date().toJSON(),
       InteractionSummary: `${operationName}`,
-      InteractionData: prayerId,
+      InteractionData: `${
+        ApollosConfig.APP.DEEP_LINK_HOST
+      }://Interactions/PrayerRequest?itemId=${prayerId}`,
     });
 
     return this.get(`/Interactions/${interactionId}`);
