59516496bc0aa093b3ac06a961959dfa790102d1
Richard Dubay <richardtdubayjr@gmail.com>
2019-05-26 14:33:59 -0400

    [DEV-3779] Fix for Active and Public Campuses (#104)
    * update rock-data-connector to only get active campuses
    
    * adds API work to only show public campuses
    
    * added test and snap
    
    * put back pointless test lines
    
    * fixed tests

--- packages/apollos-church-api/src/data/campuses/resolver.js
+++ packages/apollos-church-api/src/data/campuses/resolver.js
@@ -3,15 +3,13 @@ import { schemaMerge } from '@apollosproject/server-core';
 import { createAssetUrl } from '../utils';
 
 const resolver = {
+  Query: {
+    campuses: (root, { location }, { dataSources }) =>
+      dataSources.Campus.getPublicByLocation(location),
+  },
   Campus: {
-    latitude: ({ location }) =>
-      (typeof location.latitude === 'number' && location.latitude) || 0,
-    longitude: ({ location }) =>
-      (typeof location.longitude === 'number' && location.longitude) || 0,
     image: ({ location }) => ({
       uri: createAssetUrl(location.image),
-      width: location.image.width,
-      height: location.image.height,
     }),
   },
 };
