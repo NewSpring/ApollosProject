ca275cf6d8e767823b74cb5865b9859e184d013e
Isaac Hardy <isaac.hardy@newspring.cc>
2019-05-28 15:58:39 -0400

    Fixes Prayer Menu Images Loading (#118)
    * removes call to seperate package
    
    * remove export
    
    * ocham razor

--- packages/apollos-data-prayer/src/prayer-menu-categories/resolver.js
+++ packages/apollos-data-prayer/src/prayer-menu-categories/resolver.js
@@ -1,4 +1,18 @@
-import { createAssetUrl } from 'apollos-church-api/src/data/utils';
+import ApollosConfig from '@apollosproject/config';
+
+const { ROCK, ROCK_MAPPINGS } = ApollosConfig;
+
+const createAssetUrl = (value) => {
+  if (value.path) {
+    return value.path;
+  }
+  if (value.Key && value.AssetStorageProviderId) {
+    return `${ROCK.IMAGE_URL}/${
+      ROCK_MAPPINGS.ASSET_STORAGE_PROVIDERS[`${value.AssetStorageProviderId}`]
+    }/${value.Key}`;
+  }
+  return '';
+};
 
 const resolver = {
   Query: {
