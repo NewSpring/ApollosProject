8d00194881b697ed319d32c90ba0e4b06c3414c5
Michael Neeley <micneeley14@gmail.com>
2019-05-30 16:31:47 -0400

    Adds Custom Version Code to Android

--- packages/newspringchurchapp/appcenter-post-clone.sh
+++ packages/newspringchurchapp/appcenter-post-clone.sh
@@ -13,3 +13,6 @@ cp $(pwd)/../../yarn.lock $(pwd)/yarn.lock
 grep -o '\$.*' .env.production | sed 's/\$\(.*\)/\1/' | xargs -I {} sh -c "sed -i -e 's/\$"{}"/'$"{}"'/' .env.production"
 # Make sure ReactNativeConfig picks up values from prod env file.
 cp .env.production .env
+
+# overwrites android version code with current date and time
+sed -i "" -E "s/versionCode [0-9]+/versionCode $(date -u +"%y%m%d%H%M")/g" android/app/build.gradle
