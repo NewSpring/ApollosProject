4bda58a6393010528a9c7fa75351977a0b71a3f8
Burke Shartsis <burke@differential.com>
2019-06-06 15:59:10 -0400

    copy custom NS icons into custom icon directory 💪

--- packages/newspringchurchapp/src/theme/icons/index.js
+++ packages/newspringchurchapp/src/theme/icons/index.js
@@ -1,2 +1,8 @@
 /* Export your custom icon components here. */
-export {};
+export ArrowDown from './ArrowDown';
+export ArrowUp from './ArrowUp';
+export Pray from './Pray';
+export LeafOutline from './LeafOutline';
+export LeafSolid from './LeafSolid';
+export LeafLined from './LeafLined';
+export LeafLogo from './LeafLogo';
