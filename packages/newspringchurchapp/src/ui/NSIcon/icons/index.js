92b6151889bda94952ceed75bac7e7795c1285fd
Burke Shartsis <burke@differential.com>
2019-06-07 12:44:31 -0400

    delete NSIcon 💥 (it's no longer used)

--- packages/newspringchurchapp/src/ui/NSIcon/icons/index.js
+++
@@ -1,7 +0,0 @@
-export ArrowDown from './ArrowDown';
-export ArrowUp from './ArrowUp';
-export Pray from './Pray';
-export LeafOutline from './LeafOutline';
-export LeafSolid from './LeafSolid';
-export LeafLined from './LeafLined';
-export LeafLogo from './LeafLogo';
