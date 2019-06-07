b17e35ea59a051dcc108a0222a25ba5682763d3d
Burke Shartsis <burke@differential.com>
2019-06-07 13:47:39 -0400

    remove LeafLogo export in favor of Apollos Core BrandIcon naming

--- packages/newspringchurchapp/src/theme/icons/index.js
+++ packages/newspringchurchapp/src/theme/icons/index.js
@@ -5,4 +5,4 @@ export Pray from './Pray';
 export LeafOutline from './LeafOutline';
 export LeafSolid from './LeafSolid';
 export LeafLined from './LeafLined';
-export LeafLogo, { default as BrandIcon } from './LeafLogo';
+export BrandIcon from './LeafLogo';
