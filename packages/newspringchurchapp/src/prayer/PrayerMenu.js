aa907108994a9e32ab2333801bcc474dafe86ae6
Daniel Edwards <dan.edwards@newspring.cc>
2019-07-08 09:26:22 -0400

    Rearranged order of prayer categories

--- packages/newspringchurchapp/src/prayer/PrayerMenu.js
+++ packages/newspringchurchapp/src/prayer/PrayerMenu.js
@@ -7,6 +7,7 @@ import {
   TouchableScale,
   HorizontalTileFeed,
   PaddedView,
+  FlexedView,
   styled,
   BodyText,
   H3,
@@ -27,10 +28,26 @@ const StyledView = styled({
   justifyContent: 'flex-end',
 })(View);
 
+const StyledBodyText = styled(({ theme }) => ({
+  color: theme.colors.white,
+}))(BodyText);
+
+const StyledH3 = styled(({ theme }) => ({
+  color: theme.colors.white,
+}))(H3);
+
+const StyledGreenView = styled(({ theme }) => ({
+  backgroundColor: theme.colors.tertiary,
+}))(View);
+
+const MenuView = styled(({ theme }) => ({
+  paddingTop: theme.sizing.baseUnit * 4,
+}))(FlexedView);
+
 const Tab = ({ description, component, showAddPrayerCard }) => (
   <>
     <StyledPaddedView>
-      <BodyText>{description}</BodyText>
+      <StyledBodyText>{description}</StyledBodyText>
     </StyledPaddedView>
     {!showAddPrayerCard ? <StyledView>{component}</StyledView> : null}
   </>
@@ -83,70 +100,76 @@ class PrayerMenu extends PureComponent {
   render() {
     return (
       <SafeAreaView>
-        <AddPrayerCard
-          description={
-            'Take a moment to send a prayer request that your NewSpring Church family can pray for.'
-          }
-          {...this.props}
-        />
-        <PaddedView>
-          <H3>Pray for Others</H3>
-        </PaddedView>
-        <TabView
-          initialLayout={{
-            height: Dimensions.get('window').height,
-            width: Dimensions.get('window').width,
-          }}
-          navigationState={{ ...this.state }}
-          renderScene={({ route }) => {
-            const scenes = {};
-            this.props.categories.forEach((category) => {
-              scenes[category.key] = (
-                <Tab
-                  description={category.description}
-                  showAddPrayerCard={this.state.showAddPrayerCard}
-                  component={null}
-                />
-              );
-            });
-            return scenes[route.key];
-          }}
-          renderTabBar={(props) => (
-            <StyledFeed
-              content={this.props.categories}
-              renderItem={({ item }) => (
-                <TouchableScale
-                  key={item.key}
-                  onPress={() => {
-                    if (this.state.showAddPrayerCard) {
-                      this.animate(1);
-                      this.setState({
-                        showAddPrayerCard: false,
-                      });
-                    }
-                    this.setState({ prayerMenuItemSelected: item.key });
-                    props.jumpTo(item.key);
-                  }}
-                >
-                  <PrayerMenuCard
-                    image={item.image}
-                    overlayColor={item.overlayColor}
-                    title={item.title}
-                    selected={this.state.prayerMenuItemSelected === item.key}
-                  />
-                </TouchableScale>
-              )}
-              loadingStateObject={{
-                node: {
-                  id: 'fakeId0',
-                  isLoading: true,
-                },
+        <StyledGreenView>
+          <AddPrayerCard
+            description={
+              'Take a moment to send a prayer request that your NewSpring Church family can pray for.'
+            }
+            {...this.props}
+          />
+          <MenuView>
+            <PaddedView>
+              <StyledH3>Pray for Others</StyledH3>
+            </PaddedView>
+            <TabView
+              initialLayout={{
+                height: Dimensions.get('window').height,
+                width: Dimensions.get('window').width,
               }}
+              navigationState={{ ...this.state }}
+              renderScene={({ route }) => {
+                const scenes = {};
+                this.props.categories.forEach((category) => {
+                  scenes[category.key] = (
+                    <Tab
+                      description={category.description}
+                      showAddPrayerCard={this.state.showAddPrayerCard}
+                      component={null}
+                    />
+                  );
+                });
+                return scenes[route.key];
+              }}
+              renderTabBar={(props) => (
+                <StyledFeed
+                  content={this.props.categories}
+                  renderItem={({ item }) => (
+                    <TouchableScale
+                      key={item.key}
+                      onPress={() => {
+                        if (this.state.showAddPrayerCard) {
+                          this.animate(1);
+                          this.setState({
+                            showAddPrayerCard: false,
+                          });
+                        }
+                        this.setState({ prayerMenuItemSelected: item.key });
+                        props.jumpTo(item.key);
+                      }}
+                    >
+                      <PrayerMenuCard
+                        image={item.image}
+                        overlayColor={item.overlayColor}
+                        title={item.title}
+                        selected={
+                          this.state.prayerMenuItemSelected === item.key
+                        }
+                      />
+                    </TouchableScale>
+                  )}
+                  loadingStateObject={{
+                    node: {
+                      id: 'fakeId0',
+                      isLoading: true,
+                    },
+                  }}
+                />
+              )}
+              onIndexChange={this.handleIndexChange}
+              swipeEnabled={false}
             />
-          )}
-          onIndexChange={this.handleIndexChange}
-          swipeEnabled={false}
-        />
+          </MenuView>
+        </StyledGreenView>
       </SafeAreaView>
     );
   }
