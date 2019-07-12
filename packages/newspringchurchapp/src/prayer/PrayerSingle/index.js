23e2eb7012bdb949c45cd5a108932c8d2b99b991
Richard Dubay <richardtdubayjr@gmail.com>
2019-07-12 09:44:14 -0400

    [DEV-3906] Delete User Prayers (#178)
    * Delete prayers when the icon is tapped
    
    * Got the ActionSheet working
    
    * Updated snapshots
    
    * Absolutely position the action container
    
    * Updated the delete icon size and color
    
    * Removed unnecessary async await.
    
    * Updated snapshots

--- packages/newspringchurchapp/src/prayer/PrayerSingle/index.js
+++ packages/newspringchurchapp/src/prayer/PrayerSingle/index.js
@@ -8,7 +8,6 @@ import {
   styled,
   Touchable,
   ChannelLabel,
-  SideBySideView,
 } from '@apollosproject/ui-kit';
 import PrayerHeader from '../PrayerHeader';
 
@@ -21,6 +20,13 @@ const PrayerText = styled(({ theme }) => ({
   marginVertical: theme.sizing.baseUnit,
 }))(BodyText);
 
+const AbsolutePositionedView = styled(() => ({
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  zIndex: 2,
+}))(View);
+
 const PrayerSingle = memo(
   ({
     showHelp,
@@ -32,6 +38,7 @@ const PrayerSingle = memo(
     ...props
   }) => (
     <View>
+      <AbsolutePositionedView>{action}</AbsolutePositionedView>
       {showDate ? (
         <GreyH5>
           {prayer.enteredDateTime
@@ -39,19 +46,16 @@ const PrayerSingle = memo(
             : ''}
         </GreyH5>
       ) : null}
-      <SideBySideView>
-        {showHeader ? (
-          <PrayerHeader
-            avatarSize={avatarSize}
-            avatarSource={prayer.isAnonymous ? null : prayer.person.photo}
-            title={`Pray for ${
-              prayer.isAnonymous ? 'Request' : prayer.firstName
-            }`}
-            source={prayer.campus.name}
-          />
-        ) : null}
-        {action}
-      </SideBySideView>
+      {showHeader ? (
+        <PrayerHeader
+          avatarSize={avatarSize}
+          avatarSource={prayer.isAnonymous ? null : prayer.person.photo}
+          title={`Pray for ${
+            prayer.isAnonymous ? 'Request' : prayer.firstName
+          }`}
+          source={prayer.campus.name}
+        />
+      ) : null}
       <PrayerText>{prayer.text}</PrayerText>
       {showHelp ? (
         <Touchable
