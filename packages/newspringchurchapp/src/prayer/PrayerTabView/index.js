f26ffbc39199fc21a9a1d77dccdc46c7ea06e75a
Michael Neeley <micneeley14@gmail.com>
2019-07-15 16:27:18 -0400

    removed unnecessary file

--- packages/newspringchurchapp/src/prayer/PrayerTabView/index.js
+++ packages/newspringchurchapp/src/prayer/PrayerTabView/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import { Query } from 'react-apollo';
 import PropTypes from 'prop-types';
 import { Dimensions } from 'react-native';
 import { TabView } from 'react-native-tab-view';
@@ -8,7 +9,13 @@ import {
   TouchableScale,
 } from '@apollosproject/ui-kit';
 import PrayerMenuCard from '../PrayerMenuCard';
-import PrayerTabConnected from './PrayerTabConnected';
+
+import getUserProfile from '../../tabs/connect/getUserProfile';
+import GET_GROUP_PRAYERS from '../data/queries/getGroupPrayers';
+import GET_PRAYERS from '../data/queries/getPrayers';
+import GET_CAMPUS_PRAYERS from '../data/queries/getCampusPrayers';
+import GET_SAVED_PRAYERS from '../data/queries/getSavedPrayers';
+import PrayerTab from './PrayerTab';
 
 const StyledHorizontalTileFeed = styled({
   height: 0,
@@ -43,6 +50,12 @@ class PrayerTabView extends PureComponent {
   };
 
   render() {
+    const queries = {
+      'my-church': GET_PRAYERS,
+      'my-campus': GET_CAMPUS_PRAYERS,
+      'my-community': GET_GROUP_PRAYERS,
+      'my-saved-prayers': GET_SAVED_PRAYERS,
+    };
     return (
       <TabView
         initialLayout={{
@@ -51,7 +64,36 @@ class PrayerTabView extends PureComponent {
         }}
         navigationState={{ ...this.state }}
         renderScene={({ route }) => (
-          <PrayerTabConnected route={route} {...this.props} />
+          // <PrayerTabConnected route={route} {...this.props} />
+          <Query query={getUserProfile}>
+            {({
+              data: {
+                currentUser: {
+                  profile: { campus: { id = '' } = {} } = {},
+                } = {},
+              } = {},
+              loading,
+            }) => {
+              if (loading) return null;
+              return (
+                <Query
+                  query={queries[route.key]}
+                  variables={{ campusId: id }}
+                  fetchPolicy="cache-and-network"
+                >
+                  {({ data }) => (
+                    <PrayerTab
+                      prayers={Object.values(data)[0]}
+                      description={route.description}
+                      title={route.title}
+                      type={route.key.split('-')[1]}
+                      {...this.props}
+                    />
+                  )}
+                </Query>
+              );
+            }}
+          </Query>
         )}
         renderTabBar={(props) => (
           <StyledHorizontalTileFeed
