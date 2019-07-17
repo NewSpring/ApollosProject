940e21ad47ce6b28e6dd0d65760cd7cf99a5cb7a
Michael Neeley <micneeley14@gmail.com>
2019-07-17 17:06:54 -0400

    removed state

--- packages/newspringchurchapp/src/prayer/SaveButton/index.js
+++ packages/newspringchurchapp/src/prayer/SaveButton/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Mutation } from 'react-apollo';
 import SAVE_PRAYER from '../data/mutations/savePrayer';
@@ -6,72 +6,63 @@ import UNSAVE_PRAYER from '../data/mutations/unSavePrayer';
 import GET_SAVED_PRAYERS from '../data/queries/getSavedPrayers';
 import SaveButton from './SaveButton';
 
-class SaveButtonConnected extends React.Component {
-  state = {
-    savedButton: this.props.saved,
-  };
-
-  static propTypes = {
-    prayerID: PropTypes.string,
-    saved: PropTypes.bool,
-  };
-
-  render() {
-    return (
-      <Mutation mutation={SAVE_PRAYER}>
-        {(save) => (
-          <Mutation mutation={UNSAVE_PRAYER}>
-            {(unSave) => (
-              <SaveButton
-                saved={this.state.savedButton}
-                onPress={() => {
-                  this.setState((prevState) => ({
-                    savedButton: !prevState.savedButton,
-                  }));
-                  return this.state.savedButton
-                    ? unSave({
-                        variables: { nodeId: this.props.prayerID },
-                        update: (
-                          cache,
-                          {
-                            data: {
-                              unSavePrayer: { id },
-                            },
-                          }
-                        ) => {
-                          const data = cache.readQuery({
-                            query: GET_SAVED_PRAYERS,
-                          });
-                          const filteredPrayers = data.savedPrayers.filter(
-                            (prayer) => prayer.id !== id
-                          );
-                          cache.writeQuery({
-                            query: GET_SAVED_PRAYERS,
-                            data: { savedPrayers: filteredPrayers },
-                          });
+const SaveButtonConnected = memo(({ prayerID, saved }) => (
+  <Mutation mutation={SAVE_PRAYER}>
+    {(save) => (
+      <Mutation mutation={UNSAVE_PRAYER}>
+        {(unSave) => (
+          <SaveButton
+            saved={saved}
+            onPress={() =>
+              saved
+                ? unSave({
+                    variables: { nodeId: prayerID },
+                    update: (
+                      cache,
+                      {
+                        data: {
+                          unSavePrayer: { id },
                         },
-                      })
-                    : save({
-                        variables: { nodeId: this.props.prayerID },
-                        update: (cache, { data: { savePrayer } }) => {
-                          const data = cache.readQuery({
-                            query: GET_SAVED_PRAYERS,
-                          });
-                          data.savedPrayers.push(savePrayer);
-                          cache.writeQuery({
-                            query: GET_SAVED_PRAYERS,
-                            data,
-                          });
-                        },
+                      }
+                    ) => {
+                      const data = cache.readQuery({
+                        query: GET_SAVED_PRAYERS,
                       });
-                }}
-              />
-            )}
-          </Mutation>
+                      const filteredPrayers = data.savedPrayers.filter(
+                        (prayer) => prayer.id !== id
+                      );
+                      cache.writeQuery({
+                        query: GET_SAVED_PRAYERS,
+                        data: { savedPrayers: filteredPrayers },
+                      });
+                    },
+                  })
+                : save({
+                    variables: { nodeId: prayerID },
+                    update: (cache, { data: { savePrayer } }) => {
+                      const data = cache.readQuery({
+                        query: GET_SAVED_PRAYERS,
+                      });
+                      data.savedPrayers.push(savePrayer);
+                      cache.writeQuery({
+                        query: GET_SAVED_PRAYERS,
+                        data,
+                      });
+                    },
+                  })
+            }
+          />
         )}
       </Mutation>
-    );
-  }
-}
+    )}
+  </Mutation>
+));
+
+SaveButtonConnected.propTypes = {
+  prayerID: PropTypes.string,
+  saved: PropTypes.string,
+};
+
+SaveButtonConnected.displayName = 'SaveButtonConnected';
 
 export default SaveButtonConnected;
