79f7d5e30f78aa77823b03ded39ae631cc072d5f
Daniel Edwards <dan.edwards@newspring.cc>
2019-08-25 11:04:13 -0400

    Removed Files

--- packages/newspringchurchapp/src/linking/LinkParser.tests.js
+++
@@ -1,9 +0,0 @@
-import LinkParser from './LinkParser';
-
-test('parses the slug and fetches the id', () => {
-  expect(
-    LinkParser('fruit').toBe(
-      'newspringchurchapp://AppStackNavigator/ContentSingle?itemId=DevotionalContentItem:2e4144092c34feca80e27de85ad238e7'
-    )
-  );
-});
