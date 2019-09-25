5318ad989a7ce2bff0d09e34805441d98274b5d5
Michael Neeley <micneeley14@gmail.com>
2019-09-25 11:07:05 -0400

    adds to media items

--- packages/apollos-church-api/src/data/content-items/resolver.js
+++ packages/apollos-church-api/src/data/content-items/resolver.js
@@ -77,6 +77,9 @@ const resolver = {
   UniversalContentItem: {
     ...defaultResolvers,
   },
+  MediaContentItem: {
+    ...defaultResolvers,
+  },
 };
 
 export default resolverMerge(resolver, originalContentItem);
