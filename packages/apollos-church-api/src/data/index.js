41271204377b180ff65a63aace388cf3c252e89e
Vincent Wilson <vince@classyh.at>
2019-09-25 11:45:24 -0400

    Update API

--- packages/apollos-church-api/src/data/index.js
+++ packages/apollos-church-api/src/data/index.js
@@ -7,6 +7,7 @@ import * as Scripture from '@apollosproject/data-connector-bible';
 import * as LiveStream from '@apollosproject/data-connector-church-online';
 import * as OneSignal from '@apollosproject/data-connector-onesignal';
 import * as Pass from '@apollosproject/data-connector-passes';
+// import * as Search from '@apollosproject/data-connector-algolia-search';
 import * as Sms from '@apollosproject/data-connector-twilio';
 import { PrayerRequest, PrayerMenuCategory } from 'apollos-data-prayer';
 import {
@@ -20,6 +21,7 @@ import {
   AuthSms,
   BinaryFiles,
   RockConstants,
+  Group,
   Features as BaseFeatures,
 } from '@apollosproject/data-connector-rock';
 import * as Theme from './theme';
@@ -58,6 +60,8 @@ const data = {
   Template,
   Campus,
   BinaryFiles,
+  Group,
+  // Search,
 };
 
 const {
@@ -66,9 +70,17 @@ const {
   schema,
   context,
   applyServerMiddleware,
+  setupJobs,
 } = createApolloServerConfig(data);
 
-export { dataSources, resolvers, schema, context, applyServerMiddleware };
+export {
+  dataSources,
+  resolvers,
+  schema,
+  context,
+  applyServerMiddleware,
+  setupJobs,
+};
 
 // the upload Scalar is added
 export const testSchema = [
