2367da1f37c418783c0eb6cfa4afa1cd519f7e12
Michael Neeley <micneeley14@gmail.com>
2019-10-14 09:29:30 -0400

    removes comment

--- packages/newspringchurchapp/src/content-single/Features/TextFeature.js
+++ packages/newspringchurchapp/src/content-single/Features/TextFeature.js
@@ -14,7 +14,6 @@ const TextFeature = ({
 }) => {
   const [isPressed, press] = useState(false);
   const blanksRegex = /__(.*)/gm;
-  // TODO add this to header feature too
   const hasBlanks = body.match(blanksRegex);
   const bodyWithBlank = body.replace(blanksRegex, (match, p1) =>
     '_'.repeat(p1.length)
