74b63257407efcb25aaaaba85d14b2e309490b06
Michael Neeley <micneeley14@gmail.com>
2019-10-21 14:54:21 -0400

    Apply suggestions from code review

--- packages/newspringchurchapp/jest.setup.js
+++ packages/newspringchurchapp/jest.setup.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { NativeModules } from 'react-native';
 // We ran into an issue where SafeAreaView would break jest tests.
 
-jest.mock(
-  '../apollos-ui-kit/node_modules/react-native-safe-area-context/',
-  () => ({
-    SafeAreaConsumer: ({ children }) =>
-      children({ top: 0, bottom: 0, left: 0, right: 0 }),
-    SafeAreaProvider: ({ children }) => children,
-  })
 
 jest.mock('react-native-safe-area-context', () => ({
   SafeAreaConsumer: ({ children }) =>
