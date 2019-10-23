3fb4274f65ae63fafb4632efed8840a0e03c88ce
richarddubay <richardtdubayjr@gmail.com>
2019-10-23 13:23:34 -0400

    Gave the function a better name

--- packages/apollos-church-api/src/data/matrix-items/data-source.js
+++ packages/apollos-church-api/src/data/matrix-items/data-source.js
@@ -3,7 +3,7 @@ import RockApolloDataSource from '@apollosproject/rock-apollo-data-source';
 export default class MatrixItem extends RockApolloDataSource {
   expanded = true;
 
-  getMatrixItems = async (matrixItemGuid) => {
+  getItemsFromGuid = async (matrixItemGuid) => {
     const matrixItems = await this.request('/AttributeMatrixItems')
       .filter(`AttributeMatrix/Guid eq guid'${matrixItemGuid}'`)
       .get();
