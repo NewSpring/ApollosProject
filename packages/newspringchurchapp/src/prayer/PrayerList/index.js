da8f1a2c8a7f030956d34b1ba82b5f81d972955a
Michael Neeley <micneeley14@gmail.com>
2019-11-03 16:35:54 -0500

    Optimizes Saving Prayers

--- packages/newspringchurchapp/src/prayer/PrayerList/index.js
+++ packages/newspringchurchapp/src/prayer/PrayerList/index.js
@@ -65,24 +65,39 @@ class PrayerList extends PureComponent {
   };
 
   state = {
+    prayers: [],
     prayerIndex: 0,
     prayed: false,
+    saved: false,
   };
 
+  title = this.props.navigation.getParam('title', 'My Church');
+
+  componentDidMount() {
+    this.setState(() => {
+      const prayers = this.props.navigation.getParam(
+        'prayers',
+        cache.readQuery({ query: GET_PRAYERS }).prayers
+      );
+
+      return {
+        prayers,
+        saved: prayers[0].isSaved,
+      };
+    });
+  }
+
   render() {
-    const prayers = this.props.navigation.getParam(
-      'prayers',
-      cache.readQuery({ query: GET_PRAYERS }).prayers
-    );
-    const title = this.props.navigation.getParam('title', 'My Church');
-    const prayer = prayers[this.state.prayerIndex];
-    const isLastPrayer = this.state.prayerIndex + 1 === prayers.length;
+    const prayer = this.state.prayers[this.state.prayerIndex];
+    const isLastPrayer =
+      this.state.prayerIndex + 1 === this.state.prayers.length;
 
     const advancePrayer = (prayed = false) =>
       !isLastPrayer
         ? this.setState((prevState) => ({
             prayerIndex: prevState.prayerIndex + 1,
             prayed: prayed ? false : prevState.prayed,
+            saved: prevState.prayers[prevState.prayerIndex + 1].isSaved,
           }))
         : this.props.navigation.popToTop();
 
@@ -101,38 +116,29 @@ class PrayerList extends PureComponent {
                       <ScrollView>
                         <Header>
                           <H6>Praying for</H6>
-                          <GreenH4>{title}</GreenH4>
+                          <GreenH4>{this.title}</GreenH4>
                         </Header>
                         <StyledPrayerView>
-                          <PrayerSingle
-                            avatarSize={'medium'}
-                            navigation={this.props.navigation}
-                            prayer={prayer}
-                            action={
-                              // TODO: this query shouldn't be
-                              // necessary we need a "isSaved"
-                              // field on each prayer
-                              <Query query={GET_SAVED_PRAYERS}>
-                                {({
-                                  data: { savedPrayers } = {},
-                                  loading: savedLoading,
-                                }) => {
-                                  if (savedLoading) return null;
-                                  const savedIDs = savedPrayers.map(
-                                    (savedPrayer) => savedPrayer.id
-                                  );
-                                  return (
-                                    <SaveButton
-                                      saved={savedIDs.includes(prayer.id)}
-                                      prayerID={prayer.id}
-                                    />
-                                  );
-                                }}
-                              </Query>
-                            }
-                            showHelp
-                            showHeader
-                          />
+                          {prayer ? (
+                            <PrayerSingle
+                              avatarSize={'medium'}
+                              navigation={this.props.navigation}
+                              prayer={prayer}
+                              action={
+                                <SaveButton
+                                  toggleSavedState={() =>
+                                    this.setState((prevState) => ({
+                                      saved: !prevState.saved,
+                                    }))
+                                  }
+                                  saved={this.state.saved}
+                                  prayerID={prayer.id}
+                                />
+                              }
+                              showHelp
+                              showHeader
+                            />
+                          ) : null}
                         </StyledPrayerView>
                       </ScrollView>
                     </ScrollArea>
--- packages/newspringchurchapp/src/prayer/data/mutations/unSavePrayer.js
+++ packages/newspringchurchapp/src/prayer/data/mutations/unSavePrayer.js
@@ -4,21 +4,6 @@ export default gql`
   mutation UnSavePrayer($nodeId: String!) {
     unSavePrayer(nodeId: $nodeId) {
       id
-      firstName
-      lastName
-      isAnonymous
-      text
-      person {
-        photo {
-          uri
-        }
-      }
-      flagCount
-      campus {
-        id
-        name
-      }
-      enteredDateTime
     }
   }
 `;
