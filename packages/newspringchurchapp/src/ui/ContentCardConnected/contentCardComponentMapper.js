c18753a509220402a14abab916cec082f695b63d
Conrad VanLandingham <hi@convan.me>
2019-11-14 23:57:24 -0600

    card style tweaks

--- packages/newspringchurchapp/src/ui/ContentCardConnected/contentCardComponentMapper.js
+++ packages/newspringchurchapp/src/ui/ContentCardConnected/contentCardComponentMapper.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { get } from 'lodash';
 
-import { DefaultCard, HighlightCard } from '@apollosproject/ui-kit';
+import { DefaultCard } from '@apollosproject/ui-kit';
+
+import BrandedCard from '../BrandedCard';
 
 const contentCardComponentMapper = (props) => {
   // map typename to the the card we want to render.
   switch (get(props, '__typename')) {
-    case 'MediaContentItem':
     case 'WeekendContentItem':
-    case 'ContentSeriesContentItem':
     case 'DevotionalContentItem':
-      return <HighlightCard {...props} />;
+    case 'ContentSeriesContentItem':
+      return <BrandedCard {...props} />;
     default:
       return <DefaultCard {...props} />;
   }
