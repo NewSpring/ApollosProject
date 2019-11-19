70cf252bc3e64d878809e9aeca1c9cb7e3f13b0e
Michael Neeley <micneeley14@gmail.com>
2019-11-19 12:09:06 -0500

    fixed

--- packages/apollos-church-api/src/data/auth/data-source.js
+++ packages/apollos-church-api/src/data/auth/data-source.js
@@ -1,29 +1,6 @@
-import { AuthenticationError } from 'apollo-server';
 import { Auth as baseAuth } from '@apollosproject/data-connector-rock';
 
 export default class Auth extends baseAuth.dataSource {
-  getCurrentPerson = async ({ cookie = null } = { cookie: null }) => {
-    const { rockCookie, currentPerson } = this.context;
-    console.log('context from getCurrentPerson', this.context);
-    console.log('rockCookie from getCurrentPerson', rockCookie);
-    const userCookie = cookie || rockCookie;
-
-    if (currentPerson) {
-      return currentPerson;
-    }
-
-    if (userCookie) {
-      const request = await this.request('People/GetCurrentPerson').get({
-        options: {
-          headers: { cookie: userCookie, 'Authorization-Token': null },
-        },
-      });
-      this.context.currentPerson = request;
-      return request;
-    }
-    throw new AuthenticationError('Must be logged in');
-  };
-
   authenticate = async ({ identity, password }) => {
     // strip off email part of staff logins for Active Directory
     const newspringUsername = identity.includes('@newspring.cc')
@@ -34,7 +11,6 @@ export default class Auth extends baseAuth.dataSource {
       const sessionId = await this.createSession({ cookie });
       const token = baseAuth.generateToken({ cookie, sessionId });
       const { userToken, rockCookie } = baseAuth.registerToken(token);
-      console.log('rockCookie from authenticate', rockCookie);
       this.context.rockCookie = rockCookie;
       this.context.userToken = userToken;
       this.context.sessionId = sessionId;
