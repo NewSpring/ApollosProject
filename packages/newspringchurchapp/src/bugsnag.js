b2a7e2478362df27355277ed51e4932b369cc24b
Michael Neeley <micneeley14@gmail.com>
2019-11-21 15:22:22 -0500

    fix

--- packages/newspringchurchapp/src/bugsnag.js
+++ packages/newspringchurchapp/src/bugsnag.js
@@ -2,10 +2,10 @@ import { Client, Configuration } from 'bugsnag-react-native';
 import Config from 'react-native-config';
 import { onError } from 'apollo-link-error';
 
-const configuration = new Configuration()
-configuration.apiKey = Config.BUGSNAG_API_KEY
-configuration.releaseStage = Config.BUGSNAG_STAGE
-bugsnag = new Client(configuration);
+const configuration = new Configuration();
+configuration.apiKey = Config.BUGSNAG_API_KEY;
+configuration.releaseStage = Config.BUGSNAG_STAGE;
+const bugsnag = new Client(configuration);
 
 const bugsnagLink = onError(({ graphQLErrors, networkError, operation }) => {
   if (graphQLErrors) {
