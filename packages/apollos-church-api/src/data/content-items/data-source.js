5fecbbce611434b03f2a7405f49e67e77d81be57
Michael Neeley <micneeley14@gmail.com>
2019-11-20 19:35:27 -0500

    Adds Safety Check

--- packages/apollos-church-api/src/data/content-items/data-source.js
+++ packages/apollos-church-api/src/data/content-items/data-source.js
@@ -42,6 +42,7 @@ export default class ContentItem extends oldContentItem.dataSource {
       .get();
 
     const assetUrls = { video: '', thumbnail: '' };
+    if (!videoData.length) return assetUrls;
     JSON.parse(videoData[0].mediaData).assets.forEach((asset) => {
       if (asset.type === 'HlsVideoFile' && asset.height === 720)
         assetUrls.video = asset.url.replace('.bin', '.m3u8');
